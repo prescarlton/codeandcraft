0d202b169149bb552516990ce5bac66e55adcdab
Preston <prescarlton@gmail.com>
2023-05-19 15:31:08 -0400

    add page title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,32 +3,39 @@ import { Fira_Code } from 'next/font/google'
 import { ServerStatus } from '@/types/ServerStatus'
 import axios from 'axios'
 import Card from '@/components/Card'
+import Head from 'next/head'
 
 const firaCode = Fira_Code({ subsets: ['latin'] })
 
 export default function Home({ data }: { data: ServerStatus }) {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center  p-24 ${firaCode.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between  text-sm lg:flex">
-        <Card>Code/+/Craft</Card>
-        <Card>
-          Current Players: {data.players.now} / {data.players.max}
-        </Card>
-      </div>
-      <div className="flex gap-2">
-        {data.players.sample.map((player) => (
-          <Card key={player.id} className="w-48 h-48 flex flex-col">
-            <img
-              src={`https://mineskin.eu/helm/${player.name}`}
-              alt={player.name}
-            />
-            {player.name}
+    <>
+      <Head>
+        <title>CODE/+/CRAFT</title>
+      </Head>
+
+      <main
+        className={`flex min-h-screen flex-col items-center  p-24 ${firaCode.className}`}
+      >
+        <div className="z-10 w-full max-w-5xl items-center justify-between  text-sm lg:flex">
+          <Card>CODE/+/CRAFT</Card>
+          <Card>
+            Current Players: {data.players.now} / {data.players.max}
           </Card>
-        ))}
-      </div>
-    </main>
+        </div>
+        <div className="flex gap-2">
+          {data.players.sample.map((player) => (
+            <Card key={player.id} className="w-48 h-48 flex flex-col">
+              <img
+                src={`https://mineskin.eu/helm/${player.name}`}
+                alt={player.name}
+              />
+              {player.name}
+            </Card>
+          ))}
+        </div>
+      </main>
+    </>
   )
 }
 
